c54d1645d21857f9cb57bd16efa3f6192d6845e1
ninesky00 <jiangzho@gmail.com>
2020-10-12 19:00:49 -0500

    Add fire_upon method and tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,7 +3,7 @@ class Cell
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
-
+        @fired_upon = false
     end
 
     def empty?
@@ -14,5 +14,15 @@ class Cell
         @ship = ship
     end
 
+    def fired_upon?
+        @fired_upon
+    end
+
+    def fire_upon
+        @fired_upon = true
+        @ship.hit
+    end
+
     
+
 end
