034b445fa5f9ac3adab76374d9be22c5f3a0208e
ninesky00 <jiangzho@gmail.com>
2020-10-15 16:52:15 -0500

    Update runner file with progress on ship placement and turn interaction.

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,21 +1,9 @@
 require './lib/cell'
 require './lib/ship'
 require './lib/board'
+require 'pry'
 
-
-def start
-    puts "Welcome to BATTLESHIP"
-    puts "Enter p to play or q to quit"
-    input = gets.chomp_to_s
-    if input == "p"
-        start
-    elsif input == "q"
-        exit
-    else 
-        puts "Wrong input, please try again."
-        #loop back to welcome
-    end
-
+def start 
     player_board = Board.new
     computer_board = Board.new
     cruiser = Ship.new("Cruiser", 3)
@@ -25,50 +13,83 @@ def start
     computer_board.place(cruiser, ["A4", "B4", "C4"])
     computer_board.place(submarine, ["C2", "C3"])
 
-    #until player_board.cells.any?(obj.ship == cruiser)
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
-    puts "The Cruiser is three units long and the Submarine is two units long."
-    puts player_board.render
-    puts "Enter the coordinates for your Cruiser (3 spaces)"
-    cruiser_coordinates = gets.chomp.to_s.upcase.split(" ")
-    if player_board.valid_placement?(cruiser, cruiser_coordinates)
-        player_board.place(cruiser, cruiser_coordinates)
-    else
-        puts "Invalid coordinates, please try again."
-        #loop back to enter coordinates
+
+    cruiser_coordinates = [""]
+    until cruiser_coordinates.all? {|coord| player_board.valid_coordinate?(coord)} && 
+        player_board.valid_placement?(cruiser, cruiser_coordinates) do 
+        puts "The Cruiser is three units long and the Submarine is two units long."
+        puts player_board.render
+        puts "Enter the coordinates for your Cruiser (3 spaces)"
+        cruiser_coordinates = gets.chomp.to_s.upcase.split(" ")
+        if cruiser_coordinates.all? {|coord| player_board.valid_coordinate?(coord)} == false
+            puts "Coordinates outside of range.\n"
+        elsif player_board.valid_placement?(cruiser, cruiser_coordinates ) == false
+            puts "Invalid coordinates, please try again.\n"
+        end
     end
-    #until player_board.cells.any?(obj.ship == submarine)
-    puts player_board.render(true)
-    puts "Enter the coordinates for you Submarine (2 spaces)."
-    submarine_coordinates = gets.chomp.to_s.upcase.split(" ")
-    if player_board.valid_placement?(submarine, submarine_coordinates)
-        player_board.place(submarine, submarine_coordinates)
-    else
-        puts "Invalid coordinates, please try again."
-        #loop back to enter coordinates
+
+    player_board.place(cruiser, cruiser_coordinates)
+
+    submarine_coordinates = [""]
+    until submarine_coordinates.all? {|coord| player_board.valid_coordinate?(coord)} && 
+        player_board.valid_placement?(submarine, submarine_coordinates) do 
+        puts player_board.render(true)
+        puts "Enter the coordinates for your Submarine (2 spaces)"
+        submarine_coordinates = gets.chomp.to_s.upcase.split(" ")
+        if submarine_coordinates.all? {|coord| player_board.valid_coordinate?(coord)} == false
+            puts "Coordinates outside of range.\n\n"
+        elsif player_board.valid_placement?(submarine, submarine_coordinates) == false
+            puts "Invalid coordinates, please try again.\n"
+        end
     end
-    
+
+    player_board.place(submarine, submarine_coordinates)
+
+    player_health = 10
+    computer_health = 10 
     puts "We are ready to RRRRUUUUMMBBBLLLEEE!"
+    
+    
+    until player_health == 0 || computer_health == 0 do
+
     puts "COMPUTER BOARD".center(60, "=")
     puts computer_board.render
     puts "PLAYER BOARD".center(60, "=")
     puts player_board.render(true)
     #will have to reuse this step alot
     puts "Enter coordinate to fire"
-    pit = gets.chomp.to_s.upcase
-    if computer_board.cells.include?(pit)
-        computer_board.cells.find {|cell| cell == pit} #fire_upon
+    target = gets.chomp.to_s.upcase
+    if computer_board.cells.include?(target)
+        computer_board.cells[target].fire_upon
+        computer_board.cells[target].shot_result
     else
         puts "Please enter valid coordinate"
-        #loop back to enter coordinate
+    end
+    #implement computer target
+    player_health = [cruiser, submarine].inject(0) {|sum, ship| sum += ship.health}
+    computer_health = [cruiser, submarine].inject(0) {|sum, ship| sum += ship.health}
     end
 
-    #computer fires and loop back to player firing phase
-    #until ships are sunk loop this interaction
-    #declare victory when all ships are sunk on 1 side
-    
-    
-end
+    if computer_health == 0 
+        puts "You Won"
+    elsif player_health == 0
+        puts "I won"
+    end
 
-    
+end
+#implement global input == exit/quit to exit game
+puts "Welcome to BATTLESHIP"
+input = ""
+until input == "p" do 
+puts "Enter p to play or q to quit"
+input = gets.chomp.to_s
+if input == "p"
+    start
+elsif input == "q"
+    exit
+else 
+    puts "Wrong input, please try again."
+end
+end
