133b0a6f1024578baf80c7a2e0921d957196b42a
ninesky00 <jiangzho@gmail.com>
2020-10-21 15:20:59 -0500

    Add spaces to have consistent spacing in code blocks.

--- lib/player.rb
+++ lib/player.rb
@@ -14,12 +14,12 @@ class Player
 
   def random_coordinates(board, ship)
     coordinates = []
-    board.cells.keys.each_cons(ship.length) {|cons_keys| coordinates << cons_keys}
-    columns = board.cells.keys.map {|coord| coord.delete(coord[0])}.uniq
+    board.cells.keys.each_cons(ship.length) { |cons_keys| coordinates << cons_keys }
+    columns = board.cells.keys.map {| coord| coord.delete(coord[0]) }.uniq
     vert_choices = columns.map do |col|
-      board.cells.keys.select {|coord| coord.delete(coord[0]) == col}
+      board.cells.keys.select { |coord| coord.delete(coord[0]) == col }
     end
-    vert_choices.flatten.each_cons(ship.length) {|cons_vert| coordinates << cons_vert}
+    vert_choices.flatten.each_cons(ship.length) { |cons_vert| coordinates << cons_vert }
     computer_selection = coordinates.sample
     until board.valid_placement?(ship, computer_selection)
       computer_selection = coordinates.sample
